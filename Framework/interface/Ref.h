02fdcfa457976cefef1fefe567bbe15ea9895a51
Yutaro Iiyama <yiiyama@mit.edu>
2017-02-14 17:37:32 -0500

    implementing ConstRef separately

--- Framework/interface/Ref.h
+++ Framework/interface/Ref.h
@@ -4,7 +4,6 @@
 #include "ContainerBase.h"
 
 #include <stdexcept>
-#include <type_traits>
 
 namespace panda {
 
@@ -17,23 +16,23 @@ namespace panda {
    * proper index internally.
    * The class has pointer-like operators (-> and *) to make the implementation details transparent to users.
    */
-  template<class E, Bool_t is_const>
-  class RefT {
+  template<class E>
+  class Ref {
   public:
-    typedef RefT<E, is_const> self_type;
+    typedef Ref<E> self_type;
     typedef E value_type;
-    typedef typename std::conditional<is_const, Short_t const, Short_t>::type index_type;
+    typedef Short_t index_type;
 
     //! Default constructor.
-    RefT() {}
+    Ref() {}
     //! Standard constructor.
     /*!
      * The container must be a derived class of Array<E> or Collection<E>. There is no protection against
      * assigning a wrong type of container.
      */
-    RefT(ContainerBase const*& c, index_type& idx) : container_(&c), idx_(&idx) {}
+    Ref(ContainerBase const*& c, index_type& idx) : container_(&c), idx_(&idx) {}
     //! Copy constructor.
-    RefT(self_type const& orig) : container_(orig.container_), idx_(orig.idx_) {}
+    Ref(self_type const& orig) : container_(orig.container_), idx_(orig.idx_) {}
     //! Set the index.
     void setIndex(index_type& idx) { idx_ = &idx; }
     //! Set the container.
@@ -46,143 +45,192 @@ namespace panda {
     /*!
      * Returns a null pointer if the container is not set or the index points to an invalid location.
      */
-    value_type const* operator->() const;
+    value_type const* operator->() const
+    {
+      if (!container_ || !(*container_) || !idx_ || (*idx_) < 0)
+        return 0;
+  
+      return &static_cast<E const&>((*container_)->elemAt(*idx_));
+    }
     //! The dereference operator.
     /*!
      * Throws an invalid_argument exception if the reference is invalid.
      */
-    value_type const& operator*() const;
+    value_type const& operator*() const
+    {
+      if (!container_ || !(*container_) || !idx_ || (*idx_) < 0)
+        throw std::runtime_error("Dereferencing an invalid Ref");
+  
+      return static_cast<E const&>((*container_)->elemAt(*idx_));
+    }
     //! Copy assignment
     /*!
      * idx is copied by value. Change in the right-hand-side index value after the assignment
      * is not reflected to the left-hand-side index.
      */
-    template<Bool_t C = is_const>
-    typename std::enable_if<!C, self_type&>::type operator=(self_type const&);
+    self_type& operator=(self_type const& rhs)
+    {
+      container_ = rhs.container_;
+      if (idx_) {
+        if (rhs.idx_)
+          (*idx_) = (*rhs.idx_);
+        else
+          (*idx_) = -1;
+      }
+      return *this;
+    }
     //! Setter function
     /*!
      * Pass a pointer to a value_type object on the right hand side after the container is set.
      * If the object is found in the collector, sets the index value.
+     * Returns true on successful set.
      */
-    template<Bool_t C = is_const>
-    typename std::enable_if<!C, Bool_t>::type setRef(value_type const*);
+    Bool_t setRef(value_type const* rhs)
+    {
+      if (!rhs) {
+        container_ = 0;
+        idx_ = 0;
+      }
+  
+      if (!container_ || !(*container_) || !idx_)
+        return false;
+  
+      // loop through the container and return when a match is found
+      for ((*idx_) = 0; (*idx_) != Short_t((*container_)->size()); ++(*idx_)) {
+        if (&(*container_)->elemAt(*idx_) == rhs)
+          return true;
+      }
+  
+      // otherwise set to -1 and return false
+  
+      (*idx_) = -1;
+      return true;
+    }
     //! Validity check. Both container and idx must be valid, and idx must not be 0xffffffff.
     Bool_t isValid() const { return container_ && *container_ && idx_ && (*idx_) < (*container_)->size(); }
     //! Initializer
     /*!
      * Invalidates the index by setting it to 0xffffffff.
      */
-    template<Bool_t C = is_const>
-    typename std::enable_if<!C>::type init() { if (idx_) (*idx_) = -1; }
+    void init() { if (idx_) (*idx_) = -1; }
     //! Accessor to idx
     /*!
      * Throws a runtime_error if idx is not valid.
      */
-    template<Bool_t C = is_const>
-    typename std::enable_if<!C, Short_t&>::type idx();
+    index_type& idx()
+    {
+      if (!idx_)
+        throw std::runtime_error("Invalid index ref");
+  
+      return *idx_;
+    }
     //! Accessor to container
     /*!
      * Throws a runtime_error if container is not valid.
      */
-    ContainerBase const* container() const;
+    ContainerBase const* container() const
+    {
+      if (!container_)
+        throw std::runtime_error("Invalid container ref");
+  
+      return *container_;
+    }
     //! Pointer to object
     value_type const* get() const { return operator->(); }
 
   private:
     ContainerBase const** container_{0};
     index_type* idx_{0};
-
-    //! Hide assignment for const refs
-    template<Bool_t C = is_const>
-    typename std::enable_if<C, self_type&>::type operator=(self_type const&) { return *this; }
   };
 
-  template<class E, Bool_t is_const>
-  E const*
-  RefT<E, is_const>::operator->() const
-  {
-    if (!container_ || !(*container_) || !idx_ || (*idx_) < 0)
-      return 0;
 
-    return &static_cast<E const&>((*container_)->elemAt(*idx_));
-  }
+  //! A const version of Ref.
+  /*!
+   * Generating Ref and ConstRef from a single template has been attempted, but it was impossible to delete the assignment operator from
+   * ConstRef while enabling it in Ref (using e.g. enable_if) because the default assignment operator will take precedence.
+   */
+  template<class E>
+  class ConstRef {
+  public:
+    typedef ConstRef<E> self_type;
+    typedef E value_type;
+    typedef Short_t const index_type;
 
-  template<class E, Bool_t is_const>
-  E const&
-  RefT<E, is_const>::operator*() const
-  {
-    if (!container_ || !(*container_) || !idx_ || (*idx_) < 0)
-      throw std::runtime_error("Dereferencing an invalid Ref");
+    //! Default constructor.
+    ConstRef() {}
+    //! Standard constructor.
+    /*!
+     * The container must be a derived class of Array<E> or Collection<E>. There is no protection against
+     * assigning a wrong type of container.
+     */
+    ConstRef(ContainerBase const*& c, index_type& idx) : container_(&c), idx_(&idx) {}
+    //! Copy constructor.
+    ConstRef(self_type const& orig) : container_(orig.container_), idx_(orig.idx_) {}
 
-    return static_cast<E const&>((*container_)->elemAt(*idx_));
-  }
+    self_type& operator=(self_type const&) = delete;
+
+    //! Set the index.
+    void setIndex(index_type& idx) { idx_ = &idx; }
+    //! Set the container.
+    /*!
+     * The container must be a derived class of Array<E> or Collection<E>. There is no protection against
+     * assigning a wrong type of container.
+     */
+    void setContainer(ContainerBase const*& c) { container_ = &c; }
+    //! The arrow operator.
+    /*!
+     * Returns a null pointer if the container is not set or the index points to an invalid location.
+     */
+    value_type const* operator->() const
+    {
+      if (!container_ || !(*container_) || !idx_ || (*idx_) < 0)
+        return 0;
   
-  template<class E, Bool_t is_const>
-  template<Bool_t C/* = is_const*/>
-  typename std::enable_if<!C, RefT<E, is_const>&>::type
-  RefT<E, is_const>::operator=(self_type const& _rhs)
-  {
-    container_ = _rhs.container_;
-    if (idx_) {
-      if (_rhs.idx_)
-        (*idx_) = (*_rhs.idx_);
-      else
-        (*idx_) = -1;
+      return &static_cast<E const&>((*container_)->elemAt(*idx_));
     }
-    return *this;
-  }
-
-  template<class E, Bool_t is_const>
-  template<Bool_t C/* = is_const*/>
-  typename std::enable_if<!C, Bool_t>::type
-  RefT<E, is_const>::setRef(value_type const* _rhs)
-  {
-    if (!_rhs) {
-      container_ = 0;
-      idx_ = 0;
+    //! The dereference operator.
+    /*!
+     * Throws an invalid_argument exception if the reference is invalid.
+     */
+    value_type const& operator*() const
+    {
+      if (!container_ || !(*container_) || !idx_ || (*idx_) < 0)
+        throw std::runtime_error("Dereferencing an invalid ConstRef");
+  
+      return static_cast<E const&>((*container_)->elemAt(*idx_));
     }
-
-    if (!container_ || !(*container_) || !idx_)
-      return false;
-
-    // loop through the container and return when a match is found
-    for ((*idx_) = 0; (*idx_) != Short_t((*container_)->size()); ++(*idx_)) {
-      if (&(*container_)->elemAt(*idx_) == _rhs)
-        return true;
+    //! Validity check. Both container and idx must be valid, and idx must not be 0xffffffff.
+    Bool_t isValid() const { return container_ && *container_ && idx_ && (*idx_) < (*container_)->size(); }
+    //! Accessor to idx
+    /*!
+     * Throws a runtime_error if idx is not valid.
+     */
+    index_type& idx()
+    {
+      if (!idx_)
+        throw std::runtime_error("Invalid index ref");
+  
+      return *idx_;
     }
+    //! Accessor to container
+    /*!
+     * Throws a runtime_error if container is not valid.
+     */
+    ContainerBase const* container() const
+    {
+      if (!container_)
+        throw std::runtime_error("Invalid container ref");
+  
+      return *container_;
+    }
+    //! Pointer to object
+    value_type const* get() const { return operator->(); }
 
-    // otherwise set to -1 and return false
+  private:
+    ContainerBase const** container_{0};
+    index_type* idx_{0};
+  };
 
-    (*idx_) = -1;
-    return true;
-  }
-
-  template<class E, Bool_t is_const>
-  template<Bool_t C/* = is_const*/>
-  typename std::enable_if<!C, Short_t&>::type
-  RefT<E, is_const>::idx()
-  {
-    if (!idx_)
-      throw std::runtime_error("Invalid index ref");
-
-    return *idx_;
-  }
-
-  template<class E, Bool_t is_const>
-  ContainerBase const*
-  RefT<E, is_const>::container() const
-  {
-    if (!container_)
-      throw std::runtime_error("Invalid container ref");
-
-    return *container_;
-  }
-
-  template<class E>
-  using Ref = RefT<E, kFALSE>;
-
-  template<class E>
-  using ConstRef = RefT<E, kTRUE>;
 }
 
 #endif
