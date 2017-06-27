b708ba6e124f77a979a644d29c004194b049d4ab
Yutaro Iiyama <yiiyama@mit.edu>
2017-06-27 12:31:34 -0400

    met filter updates

--- Objects/src/MetFilters.cc
+++ Objects/src/MetFilters.cc
@@ -5,7 +5,7 @@ panda::utils::BranchList
 panda::MetFilters::getListOfBranches()
 {
   utils::BranchList blist;
-  blist += {"globalHalo16", "hbhe", "hbheIso", "ecalDeadCell", "goodVertices", "badsc", "badMuons", "duplicateMuons", "dupECALClusters", "unfixedECALHits", "badPFMuons", "badChargedHadrons"};
+  blist += {"globalHalo16", "hbhe", "hbheIso", "ecalDeadCell", "goodVertices", "badsc", "badMuons", "duplicateMuons", "unfixedECALHits", "badPFMuons", "badChargedHadrons"};
   return blist;
 }
 
@@ -24,7 +24,6 @@ panda::MetFilters::MetFilters(MetFilters const& _src) :
   badsc(_src.badsc),
   badMuons(_src.badMuons),
   duplicateMuons(_src.duplicateMuons),
-  dupECALClusters(_src.dupECALClusters),
   unfixedECALHits(_src.unfixedECALHits),
   badPFMuons(_src.badPFMuons),
   badChargedHadrons(_src.badChargedHadrons)
@@ -37,7 +36,6 @@ panda::MetFilters::MetFilters(MetFilters const& _src) :
   badsc = _src.badsc;
   badMuons = _src.badMuons;
   duplicateMuons = _src.duplicateMuons;
-  dupECALClusters = _src.dupECALClusters;
   unfixedECALHits = _src.unfixedECALHits;
   badPFMuons = _src.badPFMuons;
   badChargedHadrons = _src.badChargedHadrons;
@@ -58,7 +56,6 @@ panda::MetFilters::operator=(MetFilters const& _src)
   badsc = _src.badsc;
   badMuons = _src.badMuons;
   duplicateMuons = _src.duplicateMuons;
-  dupECALClusters = _src.dupECALClusters;
   unfixedECALHits = _src.unfixedECALHits;
   badPFMuons = _src.badPFMuons;
   badChargedHadrons = _src.badChargedHadrons;
@@ -80,7 +77,6 @@ panda::MetFilters::doSetStatus_(TTree& _tree, utils::BranchList const& _branches
   utils::setStatus(_tree, name_, "badsc", _branches);
   utils::setStatus(_tree, name_, "badMuons", _branches);
   utils::setStatus(_tree, name_, "duplicateMuons", _branches);
-  utils::setStatus(_tree, name_, "dupECALClusters", _branches);
   utils::setStatus(_tree, name_, "unfixedECALHits", _branches);
   utils::setStatus(_tree, name_, "badPFMuons", _branches);
   utils::setStatus(_tree, name_, "badChargedHadrons", _branches);
@@ -99,7 +95,6 @@ panda::MetFilters::doGetStatus_(TTree& _tree) const
   blist.push_back(utils::getStatus(_tree, name_, "badsc"));
   blist.push_back(utils::getStatus(_tree, name_, "badMuons"));
   blist.push_back(utils::getStatus(_tree, name_, "duplicateMuons"));
-  blist.push_back(utils::getStatus(_tree, name_, "dupECALClusters"));
   blist.push_back(utils::getStatus(_tree, name_, "unfixedECALHits"));
   blist.push_back(utils::getStatus(_tree, name_, "badPFMuons"));
   blist.push_back(utils::getStatus(_tree, name_, "badChargedHadrons"));
@@ -118,7 +113,6 @@ panda::MetFilters::doSetAddress_(TTree& _tree, utils::BranchList const& _branche
   utils::setAddress(_tree, name_, "badsc", &badsc, _branches, _setStatus);
   utils::setAddress(_tree, name_, "badMuons", &badMuons, _branches, _setStatus);
   utils::setAddress(_tree, name_, "duplicateMuons", &duplicateMuons, _branches, _setStatus);
-  utils::setAddress(_tree, name_, "dupECALClusters", &dupECALClusters, _branches, _setStatus);
   utils::setAddress(_tree, name_, "unfixedECALHits", &unfixedECALHits, _branches, _setStatus);
   utils::setAddress(_tree, name_, "badPFMuons", &badPFMuons, _branches, _setStatus);
   utils::setAddress(_tree, name_, "badChargedHadrons", &badChargedHadrons, _branches, _setStatus);
@@ -135,7 +129,6 @@ panda::MetFilters::doBook_(TTree& _tree, utils::BranchList const& _branches/* = 
   utils::book(_tree, name_, "badsc", "", 'O', &badsc, _branches);
   utils::book(_tree, name_, "badMuons", "", 'O', &badMuons, _branches);
   utils::book(_tree, name_, "duplicateMuons", "", 'O', &duplicateMuons, _branches);
-  utils::book(_tree, name_, "dupECALClusters", "", 'O', &dupECALClusters, _branches);
   utils::book(_tree, name_, "unfixedECALHits", "", 'O', &unfixedECALHits, _branches);
   utils::book(_tree, name_, "badPFMuons", "", 'O', &badPFMuons, _branches);
   utils::book(_tree, name_, "badChargedHadrons", "", 'O', &badChargedHadrons, _branches);
@@ -152,7 +145,6 @@ panda::MetFilters::doInit_()
   badsc = false;
   badMuons = false;
   duplicateMuons = false;
-  dupECALClusters = false;
   unfixedECALHits = false;
   badPFMuons = false;
   badChargedHadrons = false;
@@ -189,7 +181,6 @@ panda::MetFilters::dump(std::ostream& _out/* = std::cout*/) const
   _out << "badsc = " << badsc << std::endl;
   _out << "badMuons = " << badMuons << std::endl;
   _out << "duplicateMuons = " << duplicateMuons << std::endl;
-  _out << "dupECALClusters = " << dupECALClusters << std::endl;
   _out << "unfixedECALHits = " << unfixedECALHits << std::endl;
   _out << "badPFMuons = " << badPFMuons << std::endl;
   _out << "badChargedHadrons = " << badChargedHadrons << std::endl;
