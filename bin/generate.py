fb6e968bdafc2ff2eeb0ed35f4e5a2c3ceff871f
Yutaro Iiyama <yiiyama@mit.edu>
2017-08-27 06:12:13 -0400

    adapting to new LinkDef location

--- bin/generate.py
+++ bin/generate.py
@@ -252,8 +252,8 @@ for objdef in phobjects + trees:
     objdef.generate_header()
     objdef.generate_source()
 
-# write a linkdef file (not compiled by CMSSW - only for Makefile)
-linkdef = FileOutput(common.PACKDIR + '/Objects/dict/LinkDef.h')
+# write a linkdef file
+linkdef = FileOutput(common.PACKDIR + '/Objects/LinkDef.h')
 for objdef in phobjects:
     linkdef.writeline('#include "PandaTree/Objects/interface/{name}.h"'.format(name = objdef.name))
 for tree in trees:
