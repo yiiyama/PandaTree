914f1f55973882a558bdf2f10f13bd2eaaa3b7f2
dabercro <daniel.r.abercrombie@gmail.com>
2018-03-21 16:16:47 -0400

    Only download plots that are revealed.

--- RelVal/web/reveal.js
+++ RelVal/web/reveal.js
@@ -6,6 +6,21 @@ function reveal(id) {
     else
         div.style.display = "none";
 
+    if (JSON.parse(div.innerHTML) || JSON.parse(null)) {
+        var plots = JSON.parse(div.innerHTML).plots;
+        div.innerHTML = '';
+        plots.forEach(function (obj) {
+                div.innerHTML += '<span class="plot' + obj.warn + '">' +
+                    ('<span class="plot_head">%s <span class="lin_indicator" id="%s_status-lin">linear</span>' +
+                     '<span style="display:none;" class="log_indicator" id="%s_status-log">log</span> <br> ' +
+                     '<button class="log_button" onclick=swap_log("%s")>toggle log plots</button></span>').replace('%s', obj.id) +
+                    '<span id="' + obj.id + '"><a href="' + obj.base + '.pdf" target="_blank"> <img src="' + obj.base +
+                    '.png" width="100%"></a></span>' +
+                    '<span style="display:none;" id="' + obj.id + '_log"><a href="' + obj.base + '_log.pdf" target="_blank"> <img src="' + obj.base +
+                    '_log.png" width="100%"></a></span>' +
+                    '</span>';
+            });
+    }
 }
 
 function swap_log(id_base) {
