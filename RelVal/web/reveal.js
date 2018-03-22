0e0778c59a437f3686c3ca9145de49d9edc183f2
dabercro <daniel.r.abercrombie@gmail.com>
2018-03-22 10:22:19 -0400

    Fix toggle buttons.

--- RelVal/web/reveal.js
+++ RelVal/web/reveal.js
@@ -6,20 +6,23 @@ function reveal(id) {
     else
         div.style.display = "none";
 
-    if (JSON.parse(div.innerHTML) || JSON.parse(null)) {
+    try {
         var plots = JSON.parse(div.innerHTML).plots;
         div.innerHTML = '';
         plots.forEach(function (obj) {
                 div.innerHTML += '<span class="plot' + obj.warn + '">' +
-                    ('<span class="plot_head">%s <span class="lin_indicator" id="%s_status-lin">linear</span>' +
-                     '<span style="display:none;" class="log_indicator" id="%s_status-log">log</span> <br> ' +
-                     '<button class="log_button" onclick=swap_log("%s")>toggle log plots</button></span>').replace('%s', obj.id) +
+                    ('<span class="plot_head">ID <span class="lin_indicator" id="ID_status-lin">linear</span>' +
+                     '<span style="display:none;" class="log_indicator" id="ID_status-log">log</span> <br> ' +
+                     '<button class="log_button" onclick=swap_log("ID")>toggle log plots</button></span>').replace(/ID/g, obj.id) +
                     '<span id="' + obj.id + '"><a href="' + obj.base + '.pdf" target="_blank"> <img src="' + obj.base +
                     '.png" width="100%"></a></span>' +
                     '<span style="display:none;" id="' + obj.id + '_log"><a href="' + obj.base + '_log.pdf" target="_blank"> <img src="' + obj.base +
                     '_log.png" width="100%"></a></span>' +
                     '</span>';
             });
+    }
+    catch(e) {
+        // Not valid JSON. Do nothing.
     }
 }
 
